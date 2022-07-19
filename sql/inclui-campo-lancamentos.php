c0077efe4fd1b5ab5759c2d0bd92fad20cc340cf
luizbaidum <luiz.baidum@hotmail.com.br>
2022-07-18 22:47:30 -0300

    RECEBA!!
     - arrumado lançamentos em lote até 9 por vez
    
     - arrumado layout no mozilla firefox

--- sql/inclui-campo-lancamentos.php
+++ sql/inclui-campo-lancamentos.php
@@ -6,19 +6,28 @@ if(isset($_GET['despesa'])) {
 
         $_GET['numero'] = 1;
         header('location: despesas_lote.php?numero='.$_GET['numero']);
+    } else if($_GET['numero'] >= 10) {
+        
+        $_GET['numero'] = 'error';
+        header('location: despesas_lote.php?numero='.$_GET['numero']);
     } else {
-    
+
         header('location: despesas_lote.php?numero='.$_GET['numero']);
     }
+
 } else if(isset($_GET['receita'])) {
 
     if ($_GET['numero'] == '') {
 
         $_GET['numero'] = 1;
-        header('location: receitas_lote.php?numero='.$_GET['numero']);
+        header('location: despesas_lote.php?numero='.$_GET['numero']);
+    } else if($_GET['numero'] >= 10) {
+        
+        $_GET['numero'] = 'error';
+        header('location: despesas_lote.php?numero='.$_GET['numero']);
     } else {
-    
-        header('location: receitas_lote.php?numero='.$_GET['numero']);
+
+        header('location: despesas_lote.php?numero='.$_GET['numero']);
     }
 }
 
